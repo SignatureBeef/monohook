dba4885ac883b6c27b616335eb7551bd39e19259
Luke <rt.luke.s@gmail.com>
2026-05-02 22:06:04 +1000

    Refactor StartupHook

--- plugins/demo/StartupHook.cs
+++ plugins/demo/StartupHook.cs
@@ -2,22 +2,8 @@ using System;
 
 internal class StartupHook
 {
-    static readonly bool IsVerbose = Environment.GetEnvironmentVariable("MONOHOOK_VERBOSE") == "1";
-
-    static void Log(string message)
-    {
-        if (IsVerbose)
-        {
-            Console.WriteLine($"[MonoHook.NET] {message}");
-        }
-    }
-    static void Error(string message)
-    {
-        Console.Error.WriteLine($"[MonoHook.NET] ERROR: {message}");
-    }
-
     public static void Initialize()
     {
-        Console.WriteLine("[MonoHook.NET] Demo Plugin Loaded!");
+        Console.WriteLine("[ClrHook.NET] Demo Plugin Loaded!");
     }
 }
